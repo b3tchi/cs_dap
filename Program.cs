59ed91ee2c9df36049e83629958c806abd498575
jan-becka <janbecka@lego.com>
2022-02-06 11:49:53 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+class GFG {
+    // Main Method
+    public static void Main()
+    {
+        // Console.WriteLine("Waiting for debugger to attach");
+        //     while (!System.Diagnostics.Debugger.IsAttached)
+        //     {
+        //         Thread.Sleep(100);
+        //     }
+        int age;
+        string name;
+        Console.WriteLine("Enter your name:? ");
+        // using the method
+        // typecasting not needed
+        // as ReadLine returns string
+        name = Console.ReadLine();
+        // name = "abc";
+        Console.WriteLine("Enter your age: ");
+        // Converted string to int
+        // age = Convert.ToInt32(Console.ReadLine()); 
+        age = 1;
+        if (age >= 18)
+        {
+            Console.WriteLine("Hello " + name + "!"
+                        + " You can vote");
+        }
+        else {
+            Console.WriteLine("Hello " + name + "!"
+                + " Sorry you can't vote");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+```bash
+dotnet build
+```
+debug via cli
+--run arg to start imediatly
+```bash
+~/.local/share/nvim/dapinstall/dnetcs/netcoredbg/netcoredbg --run --interpreter=cli -- /usr/bin/dotnet ./bin/Debug/net6.0/main.dll
+```
+
+```bash
+~/.local/share/nvim/dapinstall/dnetcs/netcoredbg/netcoredbg --run --interpreter=vscode -- /usr/bin/dotnet ./bin/Debug/net6.0/main.dll
+```
+
+```bash
+~/.local/share/nvim/dapinstall/dnetcs/netcoredbg/netcoredbg --help
+```
